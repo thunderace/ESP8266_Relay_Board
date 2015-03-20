9bb613429920dadf5bf20a23a37263aa674a9786
thunder <thunder@debianserver.thunderace.fr>
2015-03-20 12:44:03 +0100

    import esp_mqtt git

--- mem.sh
+++ mem.sh
@@ -1,2 +1,2 @@
-OBJDUMP=/opt/xtensa-lx106-elf/bin/xtensa-lx106-elf-objdump ./mem_usage.sh build/httpd.out 81920
+OBJDUMP=/media/data/home/thunder/repo.git/ESP8266/Espressif/crosstool-NG/builds/xtensa-lx106-elf/bin/xtensa-lx106-elf-objdump ./mem_usage.sh build/httpd.out 81920
 
